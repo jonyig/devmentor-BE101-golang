919068b4b7bfcecd860948274840d4ef867bac82
neal <neal0420yang@gmail.com>
2024-03-04 23:10:49 +0800

    first take create some struct from UML and make a few adjustment about the logic

--- main.go
+++ main.go
@@ -3,3 +3,34 @@ package main
 func main() {
 	println("git test")
 }
+
+type Notifier interface {
+	Send(message, lang Language) Event
+}
+
+type EmailNotifier struct{}
+type SMSNotifier struct{}
+type TelegramNotifier struct{}
+
+func (e *EmailNotifier) Send(message, lang Language) Event {
+	return Event{}
+}
+func (e *SMSNotifier) Send(message, lang Language) Event {
+	return Event{}
+}
+func (e *TelegramNotifier) Send(message, lang Language) Event {
+	return Event{}
+}
+
+type User struct {
+	Lang Language
+}
+
+type Event struct {
+	massage string
+}
+
+type Language struct {
+	zh_TW string
+	en_US string
+}
