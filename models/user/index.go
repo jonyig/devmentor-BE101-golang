74656a2890f1b45d1bcbce62162095b875e6098c
neal <neal0420yang@gmail.com>
2024-03-21 14:58:54 +0800

    user model 01

--- models/user/index.go
+++ models/user/index.go
@@ -1,9 +1,20 @@
 package user
 
+import language "be101_golang/models/Language"
+
+// User is the method set
 type User interface {
-	Student
-	Guest
+	GetName() string
+	GetPreferredLanguage() string
 }
 
-type Student interface{}
-type Guest interface{}
+type Student struct {
+	Name              string
+	PreferredLanguage language.Language
+}
+type Guest struct {
+	Name string
+}
+
+func (s Student) GetName() string                         { return s.Name }
+func (s Student) GetPreferredLanguage() language.Language { return s.PreferredLanguage }
