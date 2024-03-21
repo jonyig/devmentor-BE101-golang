8692e487907cc84ddf4a39afcdc694d5488053e2
neal <neal0420yang@gmail.com>
2024-03-21 15:10:09 +0800

    fixed interface uniformity & let Guest struct realization User interface

--- models/user/index.go
+++ models/user/index.go
@@ -1,11 +1,11 @@
 package user
 
-import language "be101_golang/models/Language"
+import "be101_golang/models/language"
 
 // User is the method set
 type User interface {
 	GetName() string
-	GetPreferredLanguage() string
+	GetPreferredLanguage() language.Language
 }
 
 type Student struct {
@@ -18,3 +18,6 @@ type Guest struct {
 
 func (s Student) GetName() string                         { return s.Name }
 func (s Student) GetPreferredLanguage() language.Language { return s.PreferredLanguage }
+
+func (g Guest) GetName() string                         { return g.Name }
+func (g Guest) GetPreferredLanguage() language.Language { return nil }
